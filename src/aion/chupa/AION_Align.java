e8e35e75c1242d1951c8c5c927945e73ccea1b29
ChupA <kimchupa@gmail.com>
2015-07-27 02:51:43 +0900

    Added FileReader Class

--- src/aion/chupa/AION_Align.java
+++ src/aion/chupa/AION_Align.java
@@ -1,68 +1,55 @@
 package aion.chupa;
 
 import java.io.File;
-import java.io.IOException;
-
-import com.jaunt.Element;
-import com.jaunt.Elements;
-import com.jaunt.JauntException;
-import com.jaunt.UserAgent;
 
 public class AION_Align {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		try{
-			UserAgent userAgent = new UserAgent();
-			userAgent.open(new File("test/macro.html"));  //open the HTML (or XML) from a file
+		File sourceF = new File("test");
+		AION_FileList AIONFR = new AION_FileList(sourceF);
 
-			//Element htmlpage = userAgent.doc.findFirst("<HtmlPage>");  //find first div who's class matches 'images' 
-
-			//Elements elements = userAgent.doc.findEvery("<HtmlPage>");              //find all divs in the document
-			//System.out.println("Every div: " + elements.size() + " results");
-
-			//			Elements HtmlPage = userAgent.doc.findEach("<HtmlPage>");
-			//			System.out.println("Found " + HtmlPage.size() + " HtmlPage");
-			//			
-			//			for(Element HtmlPages : HtmlPage) {
-			//			
-			//				System.out.println(HtmlPages.outerHTML() + "\n---\n");
-			//				
-			//			}
-
-			Elements HtmlPage = userAgent.doc.findEach("<HtmlPage>");
-			int htmlc = 0;
-			for(Element HtmlPages : HtmlPage) {
+		AIONFR.fileList(sourceF);
+		System.out.println(AIONFR.getSourcePath());
 
 
-				int pc = 0;
-				int actc = 0;
-				htmlc++;
-
-				UserAgent inneruA = new UserAgent();
-				inneruA.openContent(HtmlPages.innerHTML());
-
-				Elements p_act = inneruA.doc.findEach("<p|act>");
-
-				for(Element p_acts : p_act) {
-					if((p_acts.outerHTML().contains("<p"))&&(!p_acts.innerHTML().matches("\\s+|^$"))){
-						pc++;
-						System.out.println("["+htmlc+"h;"+pc+"p]" + p_acts.innerHTML());
-
-					}
-					else if((p_acts.outerHTML().contains("<Act"))&&(!p_acts.innerHTML().matches("\\s+|^$"))){
-						actc++;
-						System.out.println("["+htmlc+"h;"+actc+"a]" + p_acts.innerHTML());
-					}
-				}
-			}
-
-
-		}
-		catch(JauntException | IOException e){
-			System.err.println(e);
-		}
+//		try{
+//			UserAgent userAgent = new UserAgent();
+//			userAgent.open(new File("test/macro.html"));  //open the HTML (or XML) from a file
+//
+//			Elements HtmlPage = userAgent.doc.findEach("<HtmlPage>");
+//			int htmlc = 0;
+//			for(Element HtmlPages : HtmlPage) {
+//
+//
+//				int pc = 0;
+//				int actc = 0;
+//				htmlc++;
+//
+//				UserAgent inneruA = new UserAgent();
+//				inneruA.openContent(HtmlPages.innerHTML());
+//
+//				Elements p_act = inneruA.doc.findEach("<p|act>");
+//
+//				for(Element p_acts : p_act) {
+//					if((p_acts.outerHTML().contains("<p"))&&(!p_acts.innerHTML().matches("\\s+|^$"))){
+//						pc++;
+//						System.out.println("["+htmlc+"h;"+pc+"p]" + p_acts.innerHTML());
+//
+//					}
+//					else if((p_acts.outerHTML().contains("<Act"))&&(!p_acts.innerHTML().matches("\\s+|^$"))){
+//						actc++;
+//						System.out.println("["+htmlc+"h;"+actc+"a]" + p_acts.innerHTML());
+//					}
+//				}
+//			}
+//
+//
+//		}
+//		catch(JauntException | IOException e){
+//			System.err.println(e);
+//		}
 	}
 
 }
